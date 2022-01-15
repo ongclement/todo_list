fabe4735ccca4937d27ee51b779ea0809d09df77
Clement <ongclement93@gmail.com>
2022-01-16 02:22:20 +0800

    Allow custom statuses, assignment of priority, and adding due date to Task

--- contracts/todo.sol
+++ contracts/todo.sol
@@ -5,20 +5,29 @@ pragma solidity 0.8.0;
 contract todo {
     uint public taskCounter = 0;
     mapping(uint => Task) public tasks;
+    enum Priorities { LOW, MEDIUM, HIGH}
+    Priorities constant defaultPriority = Priorities.LOW;
 
     struct Task {
         uint id;
         string description;
-        bool completed;
+        string status;
+        uint256 dueDate;
+        Priorities priority;
     }
 
     event TaskCreated(
         uint id,
-        string description
+        string description,
+        string status,
+        uint256 dueDate,
+        Priorities priority
     );
 
-    event TaskCompleted(
-        uint id
+    event TaskStatusUpdated(
+        uint id,
+        string prevStatus,
+        string newStatus
     );
 
     event TaskDeleted(
@@ -28,21 +37,44 @@ contract todo {
     constructor() {
     }
 
-    function createTask(string memory _taskDescription) public {
+    function createTask(string memory _taskDescription, string memory _status, uint256 _dueDate ) public {
         taskCounter++;
-        tasks[taskCounter] = Task(taskCounter, _taskDescription, false);
-        emit TaskCreated(taskCounter, _taskDescription);
+        tasks[taskCounter] = Task(taskCounter, _taskDescription, _status, _dueDate, defaultPriority);
+        emit TaskCreated(taskCounter, _taskDescription, _status, _dueDate, defaultPriority);
     }
 
-    function completeTask(uint _id) public {
+    function createTask(string memory _taskDescription, string memory _status, uint256 _dueDate, uint _priority ) public {
+        taskCounter++;
+        Priorities priority = Priorities(_priority);
+        tasks[taskCounter] = Task(taskCounter, _taskDescription, _status, _dueDate, priority);
+        emit TaskCreated(taskCounter, _taskDescription, _status, _dueDate, priority);
+    }
+
+    function updateTaskStatus(uint _id, string memory _status) public {
         Task memory _task = tasks[_id];
-        _task.completed = true;
+        string memory _prevStatus = _task.status;
+        _task.status = _status;
         tasks[_id] = _task;
-        emit TaskCompleted(_id);
+        emit TaskStatusUpdated(_id, _prevStatus, _status);
     }
 
     function deleteTask(uint _id) public {
         delete tasks[_id];
         emit TaskDeleted(_id);
+    }
+
+    function getPriority(uint _id) public view returns (string memory) {
+        Task memory _task = tasks[_id];
+        return getPriorityWithInt(_task.priority);
+    }
+
+    function getPriorityWithInt(Priorities _priority) internal pure returns (string memory) {
+        require(uint8(_priority) <= 2);
+
+        if (Priorities.LOW == _priority) return "LOW";
+        if (Priorities.MEDIUM == _priority) return "MEDIUM";
+        if (Priorities.HIGH == _priority) return "HIGH";
+
+        return "";
     }
 }
--- test/todo.test.js
+++ test/todo.test.js
@@ -1,6 +1,10 @@
 const todo = artifacts.require('./todo.sol')
 
 contract('todo', (accounts) => {
+    const description = 'test contract';
+    const status = 'new';
+    const dueDate = new Date().valueOf();
+
     before(async () => {
         this.todo = await todo.deployed();
     })
@@ -12,22 +16,34 @@ contract('todo', (accounts) => {
         assert.notEqual(address, undefined);
     })
 
-    it ('creates a tasks', async () => {
-        const description = 'test contract';
-        await this.todo.createTask(description);
+    it ('creates a tasks with default priority', async () => {
+        await this.todo.createTask(description, status, dueDate);
         const taskCount = await this.todo.taskCounter();
         const task = await this.todo.tasks(taskCount);
 
         assert.equal(taskCount.toNumber(), 1);
         assert.equal(task.id.toNumber(), taskCount.toNumber());
         assert.equal(task.description, description);
-        assert.equal(task.completed, false);
+        assert.equal(task.status, status);
+        assert.equal(task.dueDate, dueDate);
     })
 
-    it ('completes a task', async () => {
-        await this.todo.completeTask(1);
+    it ('creates a tasks with HIGH priority', async () => {
+        const priorityIntValue = 2;
+        await this.todo.createTask(description, status, dueDate, priorityIntValue);
+        const taskCount = await this.todo.taskCounter();
+        const task = await this.todo.tasks(taskCount);
+        const priority = await this.todo.getPriority(taskCount);
+
+        assert.equal(task.priority, priorityIntValue);
+        assert.equal(priority, "HIGH");
+    })
+
+    it ('updates status of a task', async () => {
+        const newStatus = 'reviewing';
+        await this.todo.updateTaskStatus(1, newStatus);
         const task = await this.todo.tasks(1);
-        assert.equal(task.completed, true);
+        assert.equal(task.status, newStatus);
     })
 
     it ('deletes a task', async () => {
@@ -35,6 +51,6 @@ contract('todo', (accounts) => {
         const task = await this.todo.tasks(1);
         assert.equal(task.id.toNumber(), 0);
         assert.equal(task.description, '');
-        assert.equal(task.completed, false);
+        assert.equal(task.status, '');
     })
 })
