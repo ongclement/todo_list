0506664d4b7235d4a6296a4d5ac00c5b7cfa6d6b
Clement <ongclement93@gmail.com>
2022-01-23 21:33:42 +0800

    Worked on: - adding friends list and add/remove operation - changing tasks to a private variable - getter method for tasks that filters based on access rights - private indicator in tasks to block access to friends - tests

--- contracts/todo.sol
+++ contracts/todo.sol
@@ -3,16 +3,21 @@ pragma solidity 0.8.0;
 // SPDX-License-Identifier: MIT
 
 contract todo {
-    uint public taskCounter = 0;
-    mapping(uint => Task) public tasks;
+    address public owner;
+    uint public currentIndex = 0;
+    uint public totalFriends = 0;
+    uint public totalTasks = 0;
+    mapping(uint => Task) private tasks;
     enum Priorities { LOW, MEDIUM, HIGH}
     Priorities constant defaultPriority = Priorities.LOW;
+    address[] public friends;
 
     struct Task {
         uint id;
         string description;
         string status;
         uint256 dueDate;
+        bool privateTask;
         Priorities priority;
     }
 
@@ -21,6 +26,7 @@ contract todo {
         string description,
         string status,
         uint256 dueDate,
+        bool privateTask,
         Priorities priority
     );
 
@@ -34,20 +40,36 @@ contract todo {
         uint id
     );
 
+    event FriendAdded(
+        address friend
+    );
+
+    event FriendRemoved(
+        address friend
+    );
+
     constructor() {
+        owner = msg.sender;
     }
 
-    function createTask(string memory _taskDescription, string memory _status, uint256 _dueDate ) public {
-        taskCounter++;
-        tasks[taskCounter] = Task(taskCounter, _taskDescription, _status, _dueDate, defaultPriority);
-        emit TaskCreated(taskCounter, _taskDescription, _status, _dueDate, defaultPriority);
+    modifier _ownerOnly(){
+        require(msg.sender == owner);
+        _;
     }
 
-    function createTask(string memory _taskDescription, string memory _status, uint256 _dueDate, uint _priority ) public {
-        taskCounter++;
+    function createTask(string memory _taskDescription, string memory _status, uint256 _dueDate, bool _privateTask ) public {
+        tasks[currentIndex] = Task(currentIndex, _taskDescription, _status, _dueDate, _privateTask, defaultPriority);
+        emit TaskCreated(currentIndex, _taskDescription, _status, _dueDate, _privateTask, defaultPriority);
+        currentIndex++;
+        totalTasks++;
+    }
+
+    function createTask(string memory _taskDescription, string memory _status, uint256 _dueDate, bool _privateTask, uint _priority ) public {
         Priorities priority = Priorities(_priority);
-        tasks[taskCounter] = Task(taskCounter, _taskDescription, _status, _dueDate, priority);
-        emit TaskCreated(taskCounter, _taskDescription, _status, _dueDate, priority);
+        tasks[currentIndex] = Task(currentIndex, _taskDescription, _status, _dueDate, _privateTask, priority);
+        emit TaskCreated(currentIndex, _taskDescription, _status, _dueDate, _privateTask, priority);
+        currentIndex++;
+        totalTasks++;
     }
 
     function updateTaskStatus(uint _id, string memory _status) public {
@@ -59,13 +81,61 @@ contract todo {
     }
 
     function deleteTask(uint _id) public {
-        delete tasks[_id];
+        Task memory _task = tasks[_id];
+        _task.status = 'deleted';
+        tasks[_id] = _task;
+        totalTasks--;
         emit TaskDeleted(_id);
+    }
+
+    function addFriend(address _toAdd) public {
+        friends.push(_toAdd);
+        totalFriends++;
+        emit FriendAdded(_toAdd);
+    }
+
+    function removeFriend(address _toRemove) public {
+        for(uint i=0; i<friends.length; i++) {
+            if (friends[i] == _toRemove) {
+                friends[i] = friends[friends.length - 1];
+                friends.pop();
+                totalFriends--;
+                emit FriendRemoved(_toRemove);
+            }
+        }
     }
 
     function getPriority(uint _id) public view returns (string memory) {
         Task memory _task = tasks[_id];
         return getPriorityWithInt(_task.priority);
+    }
+
+    function getTasks() public view returns (Task[] memory) {
+        require(validAddress(msg.sender), 'Read access denied');
+
+        bool[] memory tasksToReturn = new bool[](currentIndex);
+        uint localCounter = 0;
+
+        for (uint256 i=0; i< currentIndex; i++) {
+            Task memory _task = tasks[i];
+            if (keccak256(abi.encodePacked((_task.status))) != keccak256(abi.encodePacked(('deleted')))) {
+                if (msg.sender == owner || !_task.privateTask) {
+                    tasksToReturn[i] = true;
+                    localCounter++;
+                }
+            }
+        }
+
+        Task[] memory _tasks = new Task[](localCounter);
+        uint returnCounter = 0;
+        for (uint j=0; j<currentIndex; j++) {
+            if (tasksToReturn[j]) {
+                _tasks[returnCounter] = tasks[j];
+                returnCounter++;
+            }
+        }
+
+        return _tasks;
     }
 
     function getPriorityWithInt(Priorities _priority) internal pure returns (string memory) {
@@ -76,5 +146,20 @@ contract todo {
         if (Priorities.HIGH == _priority) return "HIGH";
 
         return "";
+    }
+
+    function validAddress(address toCheck) internal view returns (bool) {
+        bool valid = false;
+        if (toCheck == owner) {
+            valid = true;
+        } else {
+            for (uint i=0; i<totalFriends; i++) {
+                if (friends[i] == toCheck) {
+                    valid = true;
+                }
+            }
+        }
+
+        return valid;
     }
 }
--- test/todo.test.js
+++ test/todo.test.js
@@ -4,6 +4,7 @@ contract('todo', (accounts) => {
     const description = 'test contract';
     const status = 'new';
     const dueDate = new Date().valueOf();
+    let privateTask = false;
 
     before(async () => {
         this.todo = await todo.deployed();
@@ -11,46 +12,109 @@ contract('todo', (accounts) => {
 
     it ('deploys successfully', async () => {
         const address = await this.todo.address;
+        const owner = await this.todo.owner();
+
         assert.notEqual(address, '');
         assert.notEqual(address, null);
         assert.notEqual(address, undefined);
+        assert.equal(owner, accounts[0]);
     })
 
     it ('creates a tasks with default priority', async () => {
-        await this.todo.createTask(description, status, dueDate);
-        const taskCount = await this.todo.taskCounter();
-        const task = await this.todo.tasks(taskCount);
+        await this.todo.createTask(description, status, dueDate, privateTask);
+        const currentTaskIndex = await this.todo.currentIndex();
+        const totalTasks = await this.todo.totalTasks();
+        const tasks= await this.todo.getTasks();
+        const task= tasks[0];
 
-        assert.equal(taskCount.toNumber(), 1);
-        assert.equal(task.id.toNumber(), taskCount.toNumber());
-        assert.equal(task.description, description);
-        assert.equal(task.status, status);
-        assert.equal(task.dueDate, dueDate);
+        assert.equal(totalTasks.toNumber(), 1);
+        assert.equal(task['id'], currentTaskIndex-1);
+        assert.equal(task['description'], description);
+        assert.equal(task['status'], status);
+        assert.equal(task['dueDate'], dueDate);
+        assert.equal(task['privateTask'], privateTask);
     })
 
     it ('creates a tasks with HIGH priority', async () => {
         const priorityIntValue = 2;
-        await this.todo.createTask(description, status, dueDate, priorityIntValue);
-        const taskCount = await this.todo.taskCounter();
-        const task = await this.todo.tasks(taskCount);
-        const priority = await this.todo.getPriority(taskCount);
+        await this.todo.createTask(description, status, dueDate, privateTask, priorityIntValue);
+        const currentTaskIndex = await this.todo.currentIndex();
+        const tasks= await this.todo.getTasks();
+        const task= tasks[currentTaskIndex-1];
+        const priority = await this.todo.getPriority(currentTaskIndex-1);
 
-        assert.equal(task.priority, priorityIntValue);
+        assert.equal(task['priority'], priorityIntValue);
         assert.equal(priority, "HIGH");
     })
 
     it ('updates status of a task', async () => {
         const newStatus = 'reviewing';
-        await this.todo.updateTaskStatus(1, newStatus);
-        const task = await this.todo.tasks(1);
-        assert.equal(task.status, newStatus);
+        const currentTaskIndex = await this.todo.currentIndex();
+        await this.todo.updateTaskStatus(currentTaskIndex-1, newStatus);
+        const tasks= await this.todo.getTasks();
+        const task= tasks[currentTaskIndex-1];
+
+        assert.equal(task['status'], newStatus);
     })
 
     it ('deletes a task', async () => {
-        await this.todo.deleteTask(1);
-        const task = await this.todo.tasks(1);
-        assert.equal(task.id.toNumber(), 0);
-        assert.equal(task.description, '');
-        assert.equal(task.status, '');
+        const currentTaskIndex = await this.todo.currentIndex();
+        await this.todo.deleteTask(currentTaskIndex-1);
+        const tasks= await this.todo.getTasks();
+        const totalTasks = await this.todo.totalTasks();
+
+        assert.notEqual(currentTaskIndex, tasks.length);
+        assert.equal(totalTasks < currentTaskIndex, true);
+        assert.equal(totalTasks, tasks.length);
     })
+
+    it ('adds a friend', async () => {
+        await this.todo.addFriend(accounts[1]);
+        const friend = await this.todo.friends(0);
+
+        assert.equal(friend, accounts[1]);
+    })
+
+    it ('deletes a friend', async () => {
+        let totalFriends = await this.todo.totalFriends();
+        assert.equal(totalFriends.toNumber(), 1);
+        await this.todo.removeFriend(accounts[1]);
+        totalFriends = await this.todo.totalFriends();
+
+        assert.equal(totalFriends.toNumber(), 0);
+    })
+
+    describe("private tasks are properly hidden", function() {
+        before(async () => {
+            privateTask = true;
+            this.todo = await todo.deployed();
+            await this.todo.createTask(description, status, dueDate, privateTask);
+        });
+
+        it ('returns all tasks when called by owner', async () => {
+            const tasks = await this.todo.getTasks();
+            const totalTasks = await this.todo.totalTasks();
+
+            assert.equal(totalTasks.toNumber(), tasks.length);
+        });
+
+        it ('addresses not in friends list are denied read access', async () => {
+            let err = null;
+            try {
+                await this.todo.getTasks.call({ from: accounts[4] })
+            } catch (error) {
+                err = error;
+            }
+
+            assert.equal(err.message.endsWith('Read access denied'), true);
+        });
+
+        it ('private tasks are hidden from friends', async() => {
+            await this.todo.addFriend(accounts[4]);
+            const tasks = await this.todo.getTasks.call({ from: accounts[4] });
+            const totalTasks = await this.todo.totalTasks();
+
+            assert.notEqual(totalTasks.toNumber(), tasks.length);
+        });
+    });
 })
